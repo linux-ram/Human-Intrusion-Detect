d6122c3d3461f05434fe298c1dfb338f5d72e9dd
Ram Subramanian <sramanathan77@gmail.com>
2016-03-11 22:49:11 -0500

    Update IntrusionDetectionAlgorithm.c

--- IntrusionDetectionAlgorithm.c
+++ IntrusionDetectionAlgorithm.c
@@ -1,3 +1,16 @@
+/** Please feel free to use the code in your research and development works.
+We would appreciate a citation to the paper below when this code is helpful in obtaining results in your future publications.
+
+Publication for citation:
+(1) Abu Sajana R., R. Subramanian, P. Vijay Kumar, Syam Krishnan, Bharadwaj Amrutur, Jeena Sebastian, Malati Hegde, S. V. R. Anand,
+"A Low-Complexity Algorithm for Intrusion Detection in a PIR-Based Wireless Sensor Network,"
+In 5th International Conference on Intelligent Sensors, Sensor Networks and Information Processing (ISSNIP), pages 337–342, Dec 2009.
+
+(2) The SmartDetect WSN Team, "SmartDetect: An efficient WSN implementation for intrusion detection,"
+Communication Systems and Networks (COMSNETS), 2010 Second International Conference on, Bangalore, 2010, pp. 1-2.
+
+We suggest the user refer to the publication for implementation details. **/
+
 /*************************************/
 /* CONSTANTS USED IN THE ALGORITHM */
 #define ANALOG_DET_WNDW 128
